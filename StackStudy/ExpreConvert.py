16fa7b7f4a665ea8d2b3b714bc8b889bd80adeb7
kxwsspz2001 <62872160+kxwsspz2001@users.noreply.github.com>
2020-05-05 15:31:10 +0800

    中缀转换与计算

--- StackStudy/ExpreConvert.py
+++ StackStudy/ExpreConvert.py
@@ -8,6 +8,8 @@ def prefix_normal():pass
 def postfix_normal():pass
 def prefix_stack():pass
 def postfix_stack():pass
+def prefix_calculate():pass
+def postfix_calculate():pass
 
 def numb(stack):
     """
@@ -29,17 +31,18 @@ def express(str_=None):
     num = Stack()
     expression = []
     for i in range(len(str_)):
-        if str_[i] in '0123456789': num.push(int(str_[i]))     #如果当前字符为数字，将数字堆入栈中
-        elif str_[i] in '+-*/()':                                          #如果当前字符为符号
+        if str_[i] in '0123456789': num.push(int(str_[i]))     #检测当前字符为数字，将数字堆入栈中
+        elif str_[i] in '+-*/()':                                          #检测当前字符为符号
             if not num.isEmpty():
                 expression.append(numb(num))                  #将栈中堆放的数放入表达式
                 num = Stack()                                           #初始化栈
             expression.append(str_[i])                              #将符号放入表达式
-    if str_[-1] != ')': expression.append(numb(num))       #如果最后一位是数字，将最后栈中堆放的数放入表达式
+    if str_[-1] != ')': expression.append(numb(num))       #检测最后一位是数字，将最后栈中堆放的数放入表达式
     del num                                                                #释放内存
     return expression
 
 def judge(expression):
+    """去除多余括号"""
     #i = 0
     #for item in expression:                 #计算运算符数量
     #    if str(item) in '+-*/': i+=1
@@ -146,37 +149,92 @@ def postfix_normal(infix=None):
     return prefix_normal(infix[::-1],')','(')[::-1]
 
 def prefix_stack(infix=None):
+    """前缀的堆栈算法"""
     if infix == None: infix = express()
+    priority ={'+':1,'-':1,'*':2,'/':2,')':3}                       #设置优先级
     expression = []
     opstack = Stack()
-    for item in infix[::-1]:
-        if str(item) in '+-*/)': opstack.push(item)  
-        elif item == '(':
+    for item in infix[::-1]:                                         #倒序遍历
+        if str(item) in '+-*/)':                                     #检测为字符
+            if item == ')': opstack.push(item)                #排除右括号
+            else:
+                if opstack.isEmpty(): opstack.push(item)          #检测栈是否为空
+                else:
+                    if priority[opstack.peek()] > priority[item]:      #检测优先级
+                        expression.append(opstack.pop())             #将优先级高的取出
+                        opstack.push(item)                                 #优先级低的堆入栈中
+                    else: opstack.push(item)                              #堆入栈中
+        elif item == '(':                                                        #检测左括号，匹配右括号
             while not opstack.isEmpty():
-                if not opstack.peek() in "()": expression.append(opstack.pop())
-                else: opstack.pop()
-        else: expression.append(item)
+                if not opstack.peek() in "()": expression.append(opstack.pop())     #将算术运算符放入表达式
+                else: opstack.pop()                                          #删除括号
+        else: expression.append(item)                                   #数字添加到表达式
+    while not opstack.isEmpty():                                         #将栈中堆积的运算符放入表达式
+        expression.append(opstack.pop())
     del opstack
+    for item in expression:                                                  #删除多余括号
+        if item == ')': expression.remove(item)
     return expression[::-1]
 
 def postfix_stack(infix=None):
-    priority ={'+':1,'-':1,'*':2,'/':2}
+    """后缀的堆栈算法"""
+    priority ={'+':1,'-':1,'*':2,'/':2}                                          #设置优先级
     if infix == None: infix = express()
     expression = []
     opstack = Stack()
     for item in infix:
-        if str(item) in '+-*/': 
-            if opstack.peek() == '(': opstack.push(item)
-            elif priority[opstack.peek()] > priority[item]:
-                expression.append(opstack.pop())
-                opstack.push(item)
-            else: opstack.push(item)
-        elif item == '(': opstack.push(item)
-        elif item == ')':
-            while opstack.peek() != "(": expression.append(opstack.pop())              
-        else: expression.append(item)
-    while not opstack.isEmpty(): 
+        if str(item) in '+-*/':                                                  #检测运算符
+            if opstack.peek() == '(': pass                                  #检测栈顶为左括号 直接将运算符堆入栈中
+            else:
+                if not opstack.isEmpty():                                    #检测栈是否为空
+                    if priority[opstack.peek()] > priority[item]:        #与栈中运算符比较优先级
+                        expression.append(opstack.pop())               #取出旧运算符           
+            opstack.push(item)                                                #堆入新运算符
+        elif item == '(': opstack.push(item)                              #将左括号堆入栈中
+        elif item == ')':                                                          #检测右括号
+            while opstack.peek() != "(": expression.append(opstack.pop())       #将括号内符号加入表达式       
+        else: expression.append(item)                                     #将数字加入表达式
+    while not opstack.isEmpty():                                            #将栈中剩余运算符加入表达式
         item = opstack.pop()
         if not item in"()": expression.append(item)
     del opstack
     return expression
+
+def prefix_calculate(expression=None):
+    if expression == None: expression = prefix_stack()
+    num = Stack()
+    i = 0
+    while i < len(expression):
+        item = expression[i]
+        if str(item) in '+-*/': num.push(item)                  #运算符堆入栈中
+        else:
+            if  num.isEmpty(): num.push(item)                  #处理第一个数字
+            else:
+                if type(num.peek()) is str: num.push(item)   #检测栈顶是否为字符
+                else:
+                    num_1 = num.pop()                              #取出数字
+                    operator = num.pop()                            #取出运算符
+                    if operator == '+': expression.insert(i+1,num_1 + item)             #运算并将数字插入待处理
+                    elif operator == '-': expression.insert(i+1,num_1 - item)
+                    elif operator == '*': expression.insert(i+1,num_1 * item)
+                    elif operator == '/': expression.insert(i+1,num_1 // item)
+        i+=1
+    return num.pop()
+
+def postfix_calculate(expression=None):
+    if expression == None: expression = postfix_stack()
+    num = Stack()
+    for item in expression:
+        if type(item) is int: num.push(item)                        #将数字堆入栈中
+        else:
+            num_2 = num.pop()                                           #取出数字运算
+            num_1 = num.pop()
+            if item == '+': num.push(num_1 + num_2)
+            elif item == '-': num.push(num_1 - num_2)
+            elif item == '*': num.push(num_1 * num_2)
+            elif item == '/': num.push(num_1 // num_2)
+    return num.pop()
+
+
+
+print(postfix_calculate())
